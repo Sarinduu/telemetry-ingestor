6f6f295d8d6f8600c3c5a79ab3eec1ef7ee7c1b2
Sarindu Wijekoon <sarinduwijekoon314@gmail.com>
2026-07-21 10:12:43 +0530

    feat: enable global request validation

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigType } from '@nestjs/config';
 import { AppModule } from './app.module';
@@ -6,6 +7,16 @@ import { appConfig } from './config/app.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigType<typeof appConfig>>(appConfig.KEY);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: false },
+    }),
+  );
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(config.port);
 }
